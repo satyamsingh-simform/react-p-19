7f8117ec1ce2afec91ffdf68f0c65efa2a493326
Satyam <satyam.singh@simformsolutions.com>
2026-06-23 11:11:09 +0530

    refactor:Route logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router"
+import { BrowserRouter, Route, Routes } from "react-router"
 import { Login } from "./pages/Login"
 import { useAppDispatch, useAppSelector } from "./hooks/useStoreType"
 import { authCheck } from "./features/auth-thunk/AuthSlice"
 import { useEffect } from "react"
 import { RestaurantOptions } from "./components/restaurant-details/RestaurantOptions"
 import { RestaurantMenu } from "./components/restaurant-menu/RestaurantMenu"
+import { PublicRoute } from "./routes/PublicRoute"
+import { ProtectedRoute } from "./routes/ProtectedRoute"
 
 export const App = () => {
-  const {isAuthenticated}=useAppSelector(store=>store.auth)
 
   const dispatch=useAppDispatch();
+  const { loading } = useAppSelector((store) => store.auth);
 
   useEffect(()=>{
     const token=JSON.parse(localStorage.getItem("TOKEN:")!);
@@ -20,13 +22,22 @@ export const App = () => {
     }
   },[])
 
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={isAuthenticated?<Navigate to='/restaurant'/>:<Login/>}></Route>
-        <Route path="/restaurant" element={isAuthenticated?<RestaurantOptions/>:<Navigate to='/'/>}></Route>
-        <Route path="/city/delhi/:id" element={<RestaurantMenu/>}></Route>
-      </Routes>
+        <Route element={<PublicRoute/>}>
+          <Route path="/" element={<Login/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+        </Route>
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/restaurant" element={<RestaurantOptions/>}></Route>
+          <Route path="/city/delhi/:id" element={<RestaurantMenu/>}></Route>
+        </Route>
+        </Routes>
     </BrowserRouter>
   )
 }
